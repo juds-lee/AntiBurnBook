7400d6d822750f7812b15074478b77ffe644646d
juds-lee <judyjungmin.lee@gmail.com>
2022-05-18 14:49:37 -0400

    adding env to gitignore

--- src/App.js
+++ src/App.js
@@ -50,7 +50,7 @@ function App (){
       method: "POST",
       headers: {
         'Content-Type':  "application/json",
-        Authorization: `Bearer ${process.env.REACT_APP_API_KEY}`
+       Authorization: `Bearer ${process.env.REACT_APP_API_KEY}`
           },
       body: JSON.stringify(data)
     })
