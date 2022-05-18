f3f43807e0686a4d73f39b4ae250bb40ea28d4a0
juds-lee <judyjungmin.lee@gmail.com>
2022-05-18 12:13:49 -0400

    first push

--- src/App.js
+++ src/App.js
@@ -1,23 +1,113 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import database from './firebase';
+import { onValue, ref, push } from 'firebase/database';
+import { render } from '@testing-library/react';
 
-function App() {
+
+function App (){
+  const apiKey =  "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"
+  const [ text, setText] = useState([]);
+  const [ query, setQuery] = useState("");
+  const [ message, setMessage] = useState([]);
+  const  messageArray = [];
+  const data = {
+    prompt: `write a fiercely positive message that includes the persons ${query}.`,
+    temperature: 0.9,
+    max_tokens: 256,
+    top_p: 1,
+    frequency_penalty: 0,
+    presence_penalty: 0
+  } 
+
+
+
+  const handleAISubmit = (e) => {
+    e.preventDefault()
+    console.log("imclicked")
+    setQuery(e.target[0].value)
+  }
+  
+  const handleSaveMessage = (e) => {
+    e.preventDefault();
+    const savedMessage = e.target[0].value;
+    push(ref(database), { savedMessage });
+    console.log(savedMessage)
+    renderData()
+  }
+
+  const renderData = () => {
+    onValue(ref(database), (data) => {
+      const dataObject = data.val()
+      for (let message in dataObject) {
+        messageArray.push(dataObject[message])
+        setMessage(messageArray)
+      }
+    })
+  }
+
+   useEffect(() => {
+     renderData()
+  }, [])
+
+  useEffect(() => {
+      fetch("https://api.openai.com/v1/engines/text-curie-001/completions", {
+      method: "POST",
+      headers: {
+        'Content-Type':  "application/json",
+        Authorization: `Bearer ${apiKey}`
+          },
+      body: JSON.stringify(data)
+    })
+      .then((res) => res.json())
+      .then((resJson) => {
+      let text = resJson.choices[0].text
+      setText(text)
+      console.log(text)
+    })
+  }, [query])
+
+	useEffect(() => {
+
+
+	}, []);
+    
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <div className="App"> 
+    <header><h1>Burn Book</h1></header>
+      <form className="generateAI" onSubmit={handleAISubmit}>
+          <label htmlFor='query'></label>
+          <input
+            type='text'
+            id='query'
+            placeholder='Who would you like to write a letter about?'
+          />
+          <button type='submit'>Submit</button>
+      </form>
+      <div>
+       <form className="aiText" onSubmit={handleSaveMessage}>
+          <label htmlFor='aiText'></label>
+          <input
+            type='text'
+            id='aiText'
+            placeholder='Who would you like to message a letter about?'
+            value= {text}
+            readOnly
+          />
+          <button type='submit'>Save</button>
+      </form>
+      </div>
+      <div>
+        <ul>
+        {message.map((item) => {
+          return (
+            <li>{item.savedMessage}</li>
+          )
+        })
+
+        }
+        </ul>
+      </div>
     </div>
   );
 }
---
+++ src/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp  } from "firebase/app";
+import { getDatabase, ref } from "firebase/database";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB_9dgN0Lvl2439OvAPXugmE2kpbf6ZZ8o",
+  authDomain: "burnbook-9055f.firebaseapp.com",
+  projectId: "burnbook-9055f",
+  storageBucket: "burnbook-9055f.appspot.com",
+  messagingSenderId: "159801884433",
+  appId: "1:159801884433:web:72b551229b877d039f16b0"
+};
+// Initialize Firebase
+const firebase = initializeApp(firebaseConfig);
+const database = getDatabase(firebase);
+// const dbRef = ref(database);
+
+export default database;
